9a10f36196a7e63442bb0441e745c2411d1e6610
Afham Ahmed <afham12ahmed@gmail.com>
2026-04-03 01:07:43 +0500

    UI setup update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
 
 export default defineConfig({
   base: '/cnn-playground/',
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
